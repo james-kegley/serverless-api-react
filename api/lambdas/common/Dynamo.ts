d6f22869f466ca0b2d72ced394c99e89e47c2a18
James Kegley <james.w.kegley@gmail.com>
2021-08-30 15:11:04 +1200

    more typing

--- api/lambdas/common/Dynamo.ts
+++ api/lambdas/common/Dynamo.ts
@@ -1,14 +1,20 @@
 import AWS from 'aws-sdk';
 
-let options = {};
-if (process.env.IS_OFFLINE) {
-    options = {
-      region: 'localhost',
-      endpoint: 'http://localhost:8000',
-    };
+type Options = {
+  region?: string,
+  endpoint?: string
 }
 
-const documentClient = new AWS.DynamoDB.DocumentClient(options);
+let options: Options = {};
+
+if (process.env.IS_OFFLINE) {
+  options = {
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+  };
+}
+
+const documentClient: AWS.DynamoDB.DocumentClient = new AWS.DynamoDB.DocumentClient(options);
 
 const Dynamo = {
   async getAll(TableName: string) {
@@ -16,7 +22,7 @@ const Dynamo = {
       TableName
     };
 
-    const data = await documentClient.scan(params).promise();
+    const data: AWS.DynamoDB.DocumentClient.ScanOutput = await documentClient.scan(params).promise();
 
     if (!data) {
       throw Error(`There was an error scanning items from ${TableName}`);
@@ -34,7 +40,7 @@ const Dynamo = {
       },
     };
 
-    const data = await documentClient.get(params).promise();
+    const data: AWS.DynamoDB.DocumentClient.GetItemOutput = await documentClient.get(params).promise();
 
     if (!data || !data.Item) {
       throw Error(`There was an error fetching the data for ID of ${ID} from ${TableName}`);
@@ -44,7 +50,7 @@ const Dynamo = {
     return data.Item;
   },
 
-  async write(data: any, TableName: any) {
+  async write(data: any, TableName: string) {
     if (!data.ID) {
       throw Error('no ID on the data');
     }
@@ -54,7 +60,7 @@ const Dynamo = {
       Item: data,
     };
 
-    const res = await documentClient.put(params).promise();
+    const res: AWS.DynamoDB.DocumentClient.PutItemOutput = await documentClient.put(params).promise();
 
     if (!res) {
       throw Error(`There was an error inserting ID of ${data.ID} in table ${TableName}`);
